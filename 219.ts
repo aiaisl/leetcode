edf89162737b9c48f08462103ddc49d769deb461
luosiwei <aiaisl@outlook.com>
2016-02-17 18:15:00 +0800

    Contains Duplicate II

---
+++ 219.ts
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums: Array<number>, k: number) {
+    var origNums = nums.concat([]);
+    var numsSort = nums.sort();
+    for(var i = 0; i < origNums.length; i++) {
+        if(i < origNums.length-1 && numsSort[i] == numsSort[i + 1]) {
+            var firstNum = origNums.indexOf(origNums[i]);
+            if(origNums.indexOf(origNums[i], firstNum+1) - firstNum <= k) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+console.log(containsNearbyDuplicate([100, 10, 1, 10], 10));
+console.log(containsNearbyDuplicate([100, 10, 1, 120], 10));
+console.log(containsNearbyDuplicate([100, 10, 1, 10], 0));
+console.log(containsNearbyDuplicate([], 0));
+console.log(containsNearbyDuplicate([-1, -1], 1));
