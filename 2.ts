cce9cb0186d7537b285e9ec63e566ed998f4e3e7
luosiwei <aiaisl@outlook.com>
2016-03-15 10:09:25 +0800

    Add Two Numbers 60%

---
+++ 2.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+declare var ListNode;
+var addTwoNumbers = function(l1, l2) {
+	var result = new ListNode(null);
+	var c= result;
+    do {
+    	var v1 = l1 != null ? l1.val : 0;
+    	var v2 = l2 != null ? l2.val : 0;
+
+    	if(c.next != null) {
+    		var x = v1 + v2 + c.next.val;
+	    	if(x >= 10){
+	    		c.next = new ListNode(x%10);
+	    		c.next.next = new ListNode(Math.floor(x/10));
+	    	} else {
+	    		c.next = new ListNode((v1 + v2 + c.next.val));
+	    	}
+    	} else {
+	    	if(v1 + v2 >= 10){
+	    		c.next = new ListNode((v1 + v2)%10);
+	    		c.next.next = new ListNode(Math.floor((v1 + v2)/10));
+	    	} else {
+	    		c.next = new ListNode((v1 + v2));
+	    	}
+    	}
+    	c = c.next;
+    	l1 = l1 != null ? l1.next : null;
+    	l2 = l2 != null ? l2.next : null;
+    } while((l1 != null || l2 != null || c.next != null));
+    var r = [];
+    result = result.next;
+    while(result.next != null){
+    	r.push(result.val);
+    	result = result.next;
+    }
+    r.push(result.val);
+    return r;
+};
